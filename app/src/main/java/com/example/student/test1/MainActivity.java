56991bc47cdef8c67dffb19d180010289b37a441
narongritrit <narongrit1971@gmail.com>
2017-06-18 14:20:48 +0700

    scrollview and color

--- app/src/main/java/com/example/student/test1/MainActivity.java
+++ app/src/main/java/com/example/student/test1/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.student.test1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText,passwordEditText;
+    private TextView textView;
+    private Button button;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initail View
+        initialView();
+        textviewController();
+        //TextView Controller
+
+
+    }
+
+    private void textviewController() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent to NewRegisterActivity
+                Intent intent = new Intent(MainActivity.this,NewRegisterActivity.class);
+                startActivity(intent);//เปิดหน้าสองทับหน้าแรก
+
+            }
+        });
+    }
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+
+
+
     }
 }
